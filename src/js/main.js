8ddb3dfcbfbb9e9477fcf68e01d8bb9200059291
spectralflux <spectralflux@users.noreply.github.com>
2015-07-24 16:57:08 +1200

    capture backspace

--- src/js/main.js
+++ src/js/main.js
@@ -37,14 +37,14 @@ function create() {
 
     var style = {
         font: "19px VT323",
-        fill: 'orange',
+        fill: 'green',
         align: 'left',
         lineHeight: 19
     };
 
-    term = new Terminal(80, 25);
-    /*    term.addLine('BAD `N` RAD BBS v0.01');
-        term.addLine('---------------------');
+    term = new Terminal(30, 5);
+        term.addLine('BAD `N` RAD BBS v0.01');
+     /*   term.addLine('---------------------');
         term.addLine('---------------------');
         term.addLine('---------------------');
         term.addLine('---------------------');
@@ -58,6 +58,8 @@ function create() {
     termTextView = game.add.text(0, 0, '', style);
     termTextView.setText(term.getFormattedBuffer());
 
+    document.onkeydown = KeyCheck;  //or however you are calling your method
+
     game.input.keyboard.addCallbacks(this, null, null, keyPress);
 }
 
@@ -67,4 +69,23 @@ function update() {
 
 function keyPress(char) {
     term.addChar(char);
+}
+
+// capture non-char keys
+function KeyCheck()
+{
+   var KeyID = event.keyCode;
+   switch(KeyID)
+   {
+      case 8:
+      alert("backspace");
+      break; 
+      case 46:
+      alert("delete");
+      break;
+      default:
+      break;
+
+      //also of possible use is enter - code=13
+   }
 }
